0338c9eba41fb6eaf4982922e8a354742ef70198
Grace Hanson <hansongrace2004@gmail.com>
2025-06-22 20:15:23 -0400

    spring initializer

---
+++ src/main/java/com/backyardmate/yardmate/YardmateApplication.java
@@ -0,0 +1,13 @@
+package com.backyardmate.yardmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YardmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YardmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backyardmate/yardmate/YardmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backyardmate.yardmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YardmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
